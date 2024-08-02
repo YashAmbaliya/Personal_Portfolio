8399fbdea31e2867ec96ff4cdb91a4e94abed849
YashAmbaliya <yashambaliya266@gmail.com>
2024-08-02 19:05:12 +0530

    Solve Error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/personal_portfolio/dist/",
+	base: "/Personal_Portfolio/dist/",
 	plugins: [react()],
 	server: {
 		port: 9023,
